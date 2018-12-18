95b3b98b77897013753d0bffe4f923dba21cffab
Slavik <v.liashuk@spheremall.com>
2018-12-18 15:07:24 +0200

    quick start tutorial

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,43 @@
 |
 */
 
+use App\Task;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Validator;
+
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/tasks', function () {
+    $tasks = Task::orderBy('created_at', 'asc')->get();
+
+    return view('tasks', [
+        'tasks' => $tasks
+    ]);
+});
+
+Route::post('/task', function (Request $request) {
+    $validator = Validator::make($request->all(), [
+        'name' => 'required|max:255',
+    ]);
+
+    if ($validator->fails()) {
+        return redirect('/tasks')
+            ->withInput()
+            ->withErrors($validator);
+    }
+
+    $task = new Task;
+    $task->name = $request->name;
+    $task->save();
+
+    return redirect('/tasks');
+});
+
+Route::delete('/task/{task}', function (Task $task) {
+    $task->delete();
+
+    return redirect('/tasks');
 });
